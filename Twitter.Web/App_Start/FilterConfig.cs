16a4375bdff755ce72985c1a482feff953dc25a9
YouJinTou <dandreevd@gmail.com>
2016-02-23 15:34:59 +0200

    disabled HandleErrorAttribute

--- Twitter.Web/App_Start/FilterConfig.cs
+++ Twitter.Web/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace Twitter.Web
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            // filters.Add(new HandleErrorAttribute());
         }
     }
 }
